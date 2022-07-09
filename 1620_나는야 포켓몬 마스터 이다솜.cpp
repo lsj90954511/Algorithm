4491d7631fc7c5a56a910f650e7790e423b87dc7
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2022-07-10 03:18:19 +0900

    Add files via upload

---
+++ 1620_나는야 포켓몬 마스터 이다솜.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	string s;
+	cin >> n >> m;
+	map <int, string> pocketmon;
+	map <string, int> pocketmon2;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> s;
+		pocketmon.insert({ i, s });
+		pocketmon2.insert({ s, i });
+	}
+	for (int i = 0; i < m; i++)
+	{
+		cin >> s;
+		if (isdigit(s[0]))//¼ıÀÚ¸é
+		{
+			cout << pocketmon[stoi(s)] << "\n";
+		}
+		else
+		{
+			cout << pocketmon2[s] << "\n";
+		}
+	}
+
+	return 0;
+}
