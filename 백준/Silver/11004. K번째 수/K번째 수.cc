d8ef8a73ec158d0291a40ed9f23c8b3b14f481cd
Sojin Lee <sojin49@naver.com>
2024-05-17 12:44:11 +0900

    [Silver V] Title: K번째 수, Time: 1068 ms, Memory: 51300 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, k;
+	cin >> n >> k;
+	vector<int> v;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		v.push_back(x);
+	}
+	sort(v.begin(), v.end());
+	cout << v[k - 1];
+
+	return 0;
+}
