8bfdb8dbdc604a3159f8891dfa61e681d7723a3f
Sojin Lee <sojin49@naver.com>
2024-06-06 23:23:59 +0900

    [Silver IV] Title: 가로수, Time: 12 ms, Memory: 3308 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+
+int gcd(int a, int b)
+{
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	vector <int> v(n), d;
+
+	for (auto& i : v)
+		cin >> i;
+
+	for (int i = 0; i < n - 1; i++)
+		// 가로수들 사이의 간격 구하기
+		d.push_back(v[i + 1] - v[i]);
+
+	int g = d[0];
+	for (int i = 0; i < d.size(); i++)
+		g = gcd(g, d[i]);
+
+	int dist = v[n - 1] - v[0];
+	int ans = dist / g;
+	ans -= n - 1;
+
+	cout << ans;
+
+	return 0;
+}
