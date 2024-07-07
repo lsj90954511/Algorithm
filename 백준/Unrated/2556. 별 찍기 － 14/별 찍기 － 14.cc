16375b2e20e3bc4465bfcf0bf539b9e51e2da747
Sojin Lee <sojin49@naver.com>
2024-07-07 21:18:24 +0900

    [Unrated] Title: 별 찍기 - 14, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/2556. 별 찍기 － 14/별 찍기 － 14.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	string s;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
