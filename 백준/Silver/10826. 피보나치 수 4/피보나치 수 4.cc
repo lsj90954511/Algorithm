7077fe704b759cbf917c1e329a10654f939eec5d
Sojin Lee <sojin49@naver.com>
2024-07-15 12:10:10 +0900

    [Silver V] Title: 피보나치 수 4, Time: 220 ms, Memory: 18308 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string dp[10001];
+
+string sum(string a, string b)
+{
+	int num = 0;
+	int carry = 0;
+	string result = "";
+
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+
+	while (a.length() < b.length())
+	{
+		a += "0";
+	}
+
+	while (b.length() < a.length())
+	{
+		b += "0";
+	}
+
+	for (int i = 0; i < a.length(); i++)
+	{
+		num = ((a[i] - '0') + (b[i] - '0') + carry) % 10;
+		result += to_string(num);
+		carry = ((a[i] - '0') + (b[i] - '0') + carry) / 10;
+	}
+
+	if (carry != 0)
+	{
+		result += to_string(carry);
+	}
+
+	reverse(result.begin(), result.end());
+	return result;
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	dp[0] = "0";
+	dp[1] = "1";
+
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = sum(dp[i - 1], dp[i - 2]);
+	}
+
+	cout << dp[n];
+
+	return 0;
+}
